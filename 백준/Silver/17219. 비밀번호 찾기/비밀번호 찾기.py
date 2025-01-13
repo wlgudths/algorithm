fb413db36832d347f9edaa0fec1fde184d7bff91
Jee-Hyung Son <104887741+wlgudths@users.noreply.github.com>
2025-01-13 23:32:59 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 196 ms, Memory: 48452 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 store = dict()
 
 for _ in range(n):
-    adress, password = input().split()
+    adress, password = input().rstrip().split()
     store[adress] = password
 
 for _ in range(m):
-    site = input()
+    site = input().rstrip()
     print(store[site])
