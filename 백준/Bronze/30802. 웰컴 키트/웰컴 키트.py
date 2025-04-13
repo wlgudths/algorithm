c803beb859b31d56ddf376bca593339159280bd2
Jee-Hyung Son <104887741+wlgudths@users.noreply.github.com>
2025-04-13 15:48:49 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,15 +1,9 @@
 n = int(input())
-size = map(int, input().split())
+size = list(map(int, input().split()))
 t, p = map(int, input().split())
 
+cnt = sum(map(lambda i: i // t + (1 if i % t > 0 else 0), size))
 bundle_p, sep_p = divmod(n, p)
-
-cnt = 0
-
-for i in size:
-    cnt += i // t
-    if i % t > 0:
-        cnt += 1
 
 print(cnt)
 print(bundle_p, sep_p)
