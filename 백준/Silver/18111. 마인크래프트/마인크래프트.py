38e229dd95265d9425378eff56fba350258ace1e
Jee-Hyung Son <104887741+wlgudths@users.noreply.github.com>
2025-05-02 19:57:19 +0900

    [Silver II] Title: 마인크래프트, Time: 392 ms, Memory: 39020 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,29 +1,31 @@
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
 n, m, b = map(int, input().split())
 land = [list(map(int, input().split())) for _ in range(n)]
-
-min_h = min(min(row) for row in land)
-max_h = max(max(row) for row in land)
 ans = (float('inf'), -1)
+
+flat_land = sum(land, [])
+height_counter = Counter(flat_land)
+
+min_h = min(height_counter)
+max_h = max(height_counter)
 
 for target in range(min_h, max_h + 1):
     time = 0
     blocks = b
 
-    for i in range(n):
-        for j in range(m):
-            h = land[i][j]
+    for h, count in height_counter.items():
+        diff = h - target
+        if diff > 0:
+            time += 2 * diff * count
+            blocks += diff * count
+        elif diff < 0:
+            time += -diff * count
+            blocks -= -diff * count
 
-            if h > target:
-                time += 2 * (h - target)
-                blocks += (h - target)
-            
-            else:
-                time += (target - h)
-                blocks -= (target - h)
-    
     if blocks >= 0:
         if time < ans[0] or (time == ans[0] and target > ans[1]):
             ans = (time, target)
